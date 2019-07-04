04ad2fd36716800ddde979604ae11ff8e3d01dee
JonathanBrickwell <jonathanbrickwell@gmail.com>
2019-07-04 22:02:22 +0200

    Bug Fix: Blog

---
+++ assets/js/blog.js
@@ -0,0 +1,32 @@
+var blogs = [
+    {
+        id: 1,
+        name: 'Welcome to my first blog!',
+        description: 'Allow me to introduce myself & the content you can expect from me in my blogs...',
+        imgURL: 'assets/img/blogs/1.Welcome/1.Welcome-banner.jpg'
+    }
+]
+
+    var html = '';
+
+    for(let i = 0; i < blogs.length; i++) {
+        html += '<div class="col-lg-6 col-md-6 col-sm-12 mb-4">';
+        html += '<div class="card h-100">';
+        html += '<a href="#"><img class="card-img-top img-fluid" src="' + blogs[i].imgURL + '" alt=""></a>';
+        html += '<div class="card-body">';
+        html += '<h4 class="card-title">';
+        html += blogs[i].id + ". ";
+        html += '<a href="#">"' + blogs[i].name + '"</a>';
+        html += '</h4>';
+        html += '<p class="card-text">"' + blogs[i].description + '"</p>';
+        html += '</div>';
+        html += '</div>';
+        html += '</div>';
+    }
+    console.log(html);
+    _setData(html);
+
+function _setData(_html) {
+    var blogContentContainer = document.querySelector('.blog-content-section');
+    blogContentContainer.innerHTML = _html;
+}
