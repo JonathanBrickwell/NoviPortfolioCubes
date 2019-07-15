0f56765c4811cdbb01fdf1159df459fac575bec1
Kristijan Fištrek <kristijan.fistrek@t.ht.hr>
2019-07-15 11:20:36 +0200

    Master: Latest blog title added

--- assets/js/_services/blogs/latest-blog.js
+++ assets/js/_services/blogs/latest-blog.js
@@ -15,7 +15,7 @@ var LatestBlogs = (function() {
         html += '<div class="col-xl-4 col-lg-6 col-md-6 col-sm-12">';
             html += '<div class="featured-text text-center text-lg-left">';
                 html += '<h4 class="main-text">';
-                    html += '<a href="'+ latestBlog.destination +'" class="anchor">Latest blog</a>';
+                    html += '<a href="'+ latestBlog.destination +'" class="anchor">' + latestBlog.name + '</a>';
                 html += '</h4>';
                 html += '<p class="main-text mb-0">' + latestBlog.description + '</p>';
                 html += '<button type="button" class="btn btn-light mt-2"><a class="hp-anchor" href="'+ latestBlog.destination +'">Read more</a></button>';
