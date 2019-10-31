91654ace3a1ce7501ddd0aea560879d639debc32
Kristijan Fištrek <kristijan.fistrek@t.ht.hr>
2019-10-31 09:33:33 +0100

    #117 - Add: Music section is defined and two main projects were implemented and properly linked

--- assets/js/_components/header.js
+++ assets/js/_components/header.js
@@ -9,7 +9,7 @@ var header = (function() {
         html += '<ul class="nav justify-content-center">';
         html += '<li class="nav-item"><a href="about-me.html" class="nav-link">About me</a></li>';
         html += '<li class="nav-item"><a href="projects.html" class="nav-link">Projects</a></li>';
-        html += '<li class="nav-item"><a href="music.html" class="nav-link">Music</a></li>';
+        html += '<li class="nav-item"><a href="music/music.html" class="nav-link">Music</a></li>';
         html += '<li class="nav-item"><a href="experience.html" class="nav-link">Experience</a></li>';
         html += '<li class="nav-item"><a href="blog.html" class="nav-link">Blog</a></li>';
         html += '<li class="nav-item"><a href="contact-me.html" class="nav-link">Contact</a></li>';
@@ -19,7 +19,7 @@ var header = (function() {
         html += '<nav class="navbar dark-menu">';
         html += '<div class="navbar-container">'
         html += '<a class="navbar-brand" href="index.html">';
-        html += '<img src="assets/img/logo/Canvas.svg" width="50" height="50" alt="Homepage brand logo">';
+        html += '<img src="/assets/img/logo/Canvas.svg" width="50" height="50" alt="Homepage brand logo">';
         html += '</a>';
         html += '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">';
         html += '<div class="menu-toggle">';
